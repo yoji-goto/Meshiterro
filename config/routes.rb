3391fc4b3c6790d3c17680258684b44669e05ee0
YojiGoto <117175808+yoji-goto@users.noreply.github.com>
2022-11-17 12:06:50 +0000

    [Finish]4-13

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,8 @@
 Rails.application.routes.draw do
- resources :post_images, only: [:new, :create, :index, :show]
-  devise_for :users
+ resources :post_images, only: [:new, :create, :index, :show, :destroy]
+ devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-root to: "homes#top"
-
-get "homes/about" => "homes#about", as: 'about'
+ root to: "homes#top"
+ get "homes/about" => "homes#about", as: 'about'
 
 end
